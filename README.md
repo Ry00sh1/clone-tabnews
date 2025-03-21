e1bf084b9cb3a4825f00de9af1ea3f39938347d3
Pedro Sena <phasena@gmail.com>
2025-03-21 17:47:25 +0000

    adiciona scripts `lint:check` e `lint:fix`.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do <a href='https://www.tabnews.com.br/'>Tabnews</a> sendo realizado em conjunto com o <a href='https://www.curso.dev'>curso.dev</a>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 function Home() {
-    /*Componente React — não passa de uma função JS normal.
+  /*Componente React — não passa de uma função JS normal.
     Primeira letra maiúscula, seguindo a convenção do React para componentes.*/
-    return <h1>Quero comprar uma bota mas to em dúvida, bota em tu faz calo ou nem machuca?</h1>
-    
+  return (
+    <h1>
+      Quero comprar uma bota mas to em dúvida, bota em tu faz calo ou nem
+      machuca?
+    </h1>
+  );
 }
 
-export default Home; 
+function Teste() {
+  <h1>Teste</h1>;
+}
+
+export default Home;
 /*Exporta a função para fora do arquivo, dizendo que deve ser renderizada. Pode haver mais de um export.
 Default: identifica que é a função padrão. A partir dele, um componente chamará outro*/
